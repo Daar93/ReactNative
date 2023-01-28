f4b9eda7e2582f99ad0f6e72420dd87855a54f33
Mehmet Yilmaz <mehmet.ali.yilmaz1711@gmail.com>
2023-01-28 20:51:45 +0100

    changed textAlign

--- components/Quotes.js
+++ components/Quotes.js
@@ -1,23 +1,29 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Quote({ author, text }) {
     return (
-        <>
+        <View style={styles.container}>
             <Text style={[styles.text, styles.red]}>{text}</Text>
             <Text style={styles.author}>&mdash; {author}</Text>
-        </>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10
+    },
     text: { 
         fontSize: 25, 
         fontStyle: 'italic',
+        marginBottom: 10,
+        textAlign: "center"
     },
     red: {
         color: 'red'
     },
     author: { 
-        fontSize: 24
+        fontSize: 24,
+        textAlign: "right"
     }
 });
